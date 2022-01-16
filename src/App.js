07a4f37377de3e51889b5a6a8b351d4158a38df3
Dmitriy Dudnik <wed2288@gmail.com>
2022-01-17 01:05:57 +0200

    добавляет разметку для первого шага

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  hendleImputChange = event => {
+    this.setState({ name: event.currentTarget.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.hendleImputChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="button">Add contact</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
