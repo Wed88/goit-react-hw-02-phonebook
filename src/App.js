768dff85c39215b9af0c73626b2ae51c6caa2f7d
Dmitriy Dudnik <wed2288@gmail.com>
2022-01-21 23:41:36 +0200

    добавляет шаг с фильтром

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import shortid from 'shortid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
 
   nameImputId = shortid.generate();
   numberImputId = shortid.generate();
+  filterImputId = shortid.generate();
 
   hendleImputChange = event => {
-    const { name, number, value } = event.currentTarget;
+    const { name, value } = event.currentTarget;
 
-    this.setState({ [name]: [value] });
-    this.setState({ [number]: [value] });
+    this.setState({ [name]: value });
   };
 
   hendleSubmit = event => {
@@ -40,6 +46,10 @@ class App extends Component {
     }));
   };
 
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
   reset = () => {
     this.setState({ name: '' });
     this.setState({ number: '' });
@@ -47,6 +57,9 @@ class App extends Component {
 
   render() {
     const contacts = this.state.contacts;
+    const visibledContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
+    );
 
     return (
       <div>
@@ -78,9 +91,16 @@ class App extends Component {
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
+        <label htmlFor={this.filterImputId}>Find contacts by name</label>
+        <input
+          type="text"
+          value={this.state.filter}
+          id={this.filterImputId}
+          onChange={this.changeFilter}
+        ></input>
         {this.state.contacts && (
           <ul>
-            {contacts.map(contact => (
+            {visibledContacts.map(contact => (
               <li key={contact.id}>
                 <p>
                   {contact.name}:&nbsp; {contact.number}
